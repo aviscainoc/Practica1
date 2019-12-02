788c940f22371e2e33325eccd0853cd12f0b9be5
domerchan <domerchan@users.noreply.github.com>
2019-12-01 23:16:21 -0500

    Create Libro.java
    clase libro en el Client

---
+++ BibEJBclient/src/main/java/modelo/Libro.java
@@ -0,0 +1,51 @@
+package modelo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Libro implements Serializable {
+	
+	private static final long serialVersionUID = -20336965027624681L;
+	
+	private int li_codigo;
+	private String li_titulo;
+	private String autor;
+	private Date fecha_publicacion;
+	
+	
+	public int getLi_codigo() {
+		return li_codigo;
+	}
+	public void setLi_codigo(int li_codigo) {
+		this.li_codigo = li_codigo;
+	}
+	public String getLi_titulo() {
+		return li_titulo;
+	}
+	public void setLi_titulo(String li_titulo) {
+		this.li_titulo = li_titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public Date getFecha_publicacion() {
+		return fecha_publicacion;
+	}
+	public void setFecha_publicacion(Date fecha_publicacion) {
+		this.fecha_publicacion = fecha_publicacion;
+	}
+	@Override
+	public String toString() {
+		return "Libro [li_codigo=" + li_codigo + ", li_titulo=" + li_titulo + ", autor=" + autor
+				+ ", fecha_publicacion=" + fecha_publicacion + "]";
+	}
+	
+	
+	
+	
+	
+
+}
