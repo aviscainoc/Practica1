11342dd97b7de6b7d8612e6a49eb8d54ab31172e
domerchan <domerchan@users.noreply.github.com>
2019-12-01 22:54:13 -0500

    Update Interfaz.java
    creación interfaz gráfica

--- BibEJBclient/src/main/java/vista/Interfaz.java
+++ BibEJBclient/src/main/java/vista/Interfaz.java
@@ -1,5 +1,18 @@
 package vista;
 
-public class Interfaz {
+import javax.swing.JFrame;
 
+public class Interfaz {
+	
+	public JFrame frame;
+
+	public Interfaz() {
+		initialise();
+	}
+	
+	private void initialise() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
