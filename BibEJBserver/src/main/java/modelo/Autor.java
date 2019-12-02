8219ae25346131a5f77c61f250b98a263b3d2b5b
domerchan <domerchan@users.noreply.github.com>
2019-12-02 11:18:32 -0500

    insertar autores desde el servidor

--- BibEJBserver/src/main/java/modelo/Autor.java
+++ BibEJBserver/src/main/java/modelo/Autor.java
@@ -2,7 +2,12 @@ package modelo;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Autor {
+	@Id
 	private int au_codigo;
 	private String au_nombre;
 	private String au_fechaNacimiento;
