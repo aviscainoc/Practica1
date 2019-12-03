67e34b6cc6114d731b660c0f283a2063a155b7df
pablocalle1998 <pablocalle1998@users.noreply.github.com>
2019-12-03 10:38:20 -0500

    correccion de errores

--- BibEJBserver/src/main/java/negocio/GestionEditorialLocal.java
+++ BibEJBserver/src/main/java/negocio/GestionEditorialLocal.java
@@ -1,17 +1,18 @@
 package negocio;
 
-import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Local;
 
-import modelo.Autor;
 import modelo.Editorial;
+
+
 
 @Local
 public interface GestionEditorialLocal {
-	public void guardarEditorial(int ed_codigo, String ed_pais, String ed_nombre, String ed_fundador, Date ed_fundacion);
+	public void guardarEditorial(int ed_codigo, String ed_pais, String ed_nombre, String ed_fundador, String ed_fundacion);
 	public List<Editorial> getEditoriales();
-	
-	public void eliminar(int ed_codigo);
+	public void update(int ed_codigo, String ed_pais, String ed_nombre, String ed_fundador, String ed_fundacion);
+	public void remove(int ed_codigo);
+	public List<String> getNombreEditoriales();
 }
--- BibEJBserver/src/main/java/vista/GestioneEditorialesBean.java
+++ BibEJBserver/src/main/java/vista/GestioneEditorialesBean.java
@@ -1,33 +1,34 @@
 package vista;
 
-import java.util.Date;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.inject.Inject;
 
+import modelo.Autor;
 import modelo.Editorial;
+import negocio.GestionAutoresLocal;
 import negocio.GestionEditorialLocal;
 
 @ManagedBean
 public class GestioneEditorialesBean {
 	@Inject
-	private GestionEditorialLocal gl;
+	private GestionEditorialLocal ge;
 	
 	private int ed_codigo;
 	private String ed_pais;
 	private String ed_nombre;
 	private String ed_fundador;
-	private Date ed_fundacion;
+	private String ed_fundacion;
 	
 	private List<Editorial> editoriales;
 
-	public GestionEditorialLocal getGl() {
-		return gl;
+	public GestionEditorialLocal getGe() {
+		return ge;
 	}
 
-	public void setGl(GestionEditorialLocal gl) {
-		this.gl = gl;
+	public void setGe(GestionEditorialLocal ge) {
+		this.ge = ge;
 	}
 
 	public int getEd_codigo() {
@@ -37,7 +38,7 @@ public class GestioneEditorialesBean {
 	public void setEd_codigo(int ed_codigo) {
 		this.ed_codigo = ed_codigo;
 	}
-	
+
 	public String getEd_pais() {
 		return ed_pais;
 	}
@@ -45,7 +46,7 @@ public class GestioneEditorialesBean {
 	public void setEd_pais(String ed_pais) {
 		this.ed_pais = ed_pais;
 	}
-
+	
 	public String getEd_nombre() {
 		return ed_nombre;
 	}
@@ -53,7 +54,7 @@ public class GestioneEditorialesBean {
 	public void setEd_nombre(String ed_nombre) {
 		this.ed_nombre = ed_nombre;
 	}
-	
+
 	public String getEd_fundador() {
 		return ed_fundador;
 	}
@@ -61,35 +62,50 @@ public class GestioneEditorialesBean {
 	public void setEd_fundador(String ed_fundador) {
 		this.ed_fundador = ed_fundador;
 	}
-	
-	
-	public Date getEd_fundacion() {
+
+	public String getEd_fundacion() {
 		return ed_fundacion;
 	}
 
-	public void setEd_fundacion(Date ed_fundacion) {
+	public void setEd_fundacion(String ed_fundacion) {
 		this.ed_fundacion = ed_fundacion;
 	}
-
 	
+	public List<Editorial> getEditoriales() {
+		return editoriales;
+	}
+
+	public void setEditoriales(List<Editorial> editoriales) {
+		this.editoriales = editoriales;
+	}
 	
 	public String guardarEditorial() {
-		System.out.println(ed_codigo + " "+ ed_pais + " " + ed_nombre + " "+ ed_fundador + " " + ed_fundacion + " " );
-		gl.guardarEditorial(ed_codigo, ed_pais, ed_nombre, ed_fundador, ed_fundacion);
-		editoriales = gl.getEditoriales();
-		return "Autor registrado";
+		System.out.println(ed_codigo + " " + ed_pais+ " " + ed_nombre + " " + ed_fundador + " " + ed_fundacion);
+		ge.guardarEditorial(ed_codigo, ed_pais, ed_nombre, ed_fundador, ed_fundacion);
+		editoriales = ge.getEditoriales();
+		return "Editorial registrada";
 	}
 	
 	public List<Editorial> recuperarEditoriales() {
-		editoriales = gl.getEditoriales();
+		editoriales = ge.getEditoriales();
 		return editoriales;
 	}
 	
-	public void eliminar() {
-		gl.eliminar(ed_codigo);
+	public void eliminarEditoriales() {
+		ge.remove(ed_codigo);
+	}
+	
+	public void actualizarEditorial() {
+		ge.update(ed_codigo, ed_pais, ed_nombre, ed_fundador, ed_fundacion);
+	}
+
+	public List<String> getNombreEditoriales(){
+		List<String> editoriales=ge.getNombreEditoriales();
+		return editoriales;
 	}
 	
 }
+
 
 
 
